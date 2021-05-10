2d7869b40a2bdb423f1d9e5f29ad526ea11469a5
Stuart Parkington <stuart.parkington@citrix.com>
2021-05-10 15:34:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Multipass-cxcli
+ A cloud-init file and simple bash script to set up the Citrix Cloud cx cli tool in a Multipass VM
---
+++ cxcli_vm.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+OP="$1"
+VM_NAME="$2"
+
+
+if [ "$VM_NAME" = "" ]; then
+	VM_NAME="cxcli"
+fi
+
+if [ "$OP" = "setup" ]; then
+	multipass launch -n $VM_NAME --cloud-init ./cloud-init.yaml && multipass shell $VM_NAME
+elif [ "$OP" = "delete" ]; then
+	multipass stop $VM_NAME && multipass delete $VM_NAME && multipass purge
+else 
+	echo "Error!"
+	echo "No or unknown switch supplied."
+	echo
+	echo "cxcli_vm.sh <setup | delete> [name_of_vm]"
+	echo
+	echo "If not supplied the VM name will default to 'cxcli'"
+fi
