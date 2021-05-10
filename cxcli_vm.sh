4d46a42444d2ab1e3f48f47c1902cea684259513
Stuart Parkington <stuart.parkington@citrix.com>
2021-05-10 16:05:16 +0100

    Updated bash file

--- cxcli_vm.sh
+++ cxcli_vm.sh
@@ -2,13 +2,20 @@
 OP="$1"
 VM_NAME="$2"
 
-
 if [ "$VM_NAME" = "" ]; then
 	VM_NAME="cxcli"
 fi
 
+if [ !-d ./shared ]; then
+	mkdir ./shared
+	touch ./shared/mounted.txt
+	echo "This is mounted from the host computer!" > ./shared/mounted.txt
+fi
+
 if [ "$OP" = "setup" ]; then
-	multipass launch -n $VM_NAME --cloud-init ./cloud-init.yaml && multipass shell $VM_NAME
+	multipass launch -n $VM_NAME --cloud-init ./cloud-init.yaml
+	multipass mount ./shared $VM_NAME
+	multipass shell $VM_NAME
 elif [ "$OP" = "delete" ]; then
 	multipass stop $VM_NAME && multipass delete $VM_NAME && multipass purge
 else 
